de6c7d2218c96ccb7d5c8988f9721b6b61a759ed
elishambiro <78095379+elishambiro@users.noreply.github.com>
2022-02-19 18:21:30 +0200

    Create e2e.py

---
+++ e2e.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from time import sleep
+
+
+def test_scores_service(my_driver):
+    my_driver.get("http://localhost:5000")
+    my_driver.maximize_window()
+    test = my_driver.find_element_by_xpath('//*[@id="tbody"]/tr[1]/td[2]').text
+    if 1 <= int(test) <= 1000:
+        return True
+    else:
+        return False
+
+
+def main_function():
+    my_driver = webdriver.Chrome()
+    bool = test_scores_service(my_driver)
+    if bool:
+        return -1
+    else:
+        return 0
+
+print(main_function())
